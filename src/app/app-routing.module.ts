50d56a19d586d0f2b21b0a4bf450d1aee819646a
Romit Modi <modiromit7@gmail.com>
2021-05-26 15:47:38 +0530

    navigation is activated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
 import { BondsViewComponent } from "./bonds/bonds-view/bonds-view.component";
 import { MutualFundsViewComponent } from "./mutual-fund/mutual-funds-view/mutual-funds-view.component";
 import { SharesViewComponent } from "./shares/shares-view/shares-view.component";
 import { SummaryViewComponent } from "./summary/summary-view/summary-view.component";
 
 const routes: Route[] = [
-    { path: '', component: AppComponent },
+    { path: '', redirectTo: '', pathMatch: 'full' },
     { path: 'summary', component: SummaryViewComponent },
     { path: 'shares', component: SharesViewComponent },
     { path: 'mutual-funds', component: MutualFundsViewComponent },
